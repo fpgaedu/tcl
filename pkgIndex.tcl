1d6a6217b9c1132c72f134f9380c7fe3df2c61d3
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2017-02-16 19:09:11 +0100

    added new assertion modules

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,2 +1,4 @@
 package ifneeded fpgaedu::json 1.0 [list source [file join $dir json.tcl]]
 package ifneeded fpgaedu::jsonrpc 1.0 [list source [file join $dir jsonrpc.tcl]]
+package ifneeded fpgaedu::assert 1.0 [list source [file join $dir assert.tcl]]
+package ifneeded fpgaedu::assert::json 1.0 [list source [file join $dir assertjson.tcl]]
