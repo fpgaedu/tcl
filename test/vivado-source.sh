c3550c497645a6712ea595f0099286eff562cc6f
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2017-02-21 21:03:47 +0100

    Extended source script to remove webtalk files

--- test/vivado-source.sh
+++ test/vivado-source.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 vivado -mode batch -nolog -nojournal -notrace -source $@
+# Clean up webtalk log and journal. The generation of these files is 
+# unaffected by the -nolog and -nojournal command line arguments.
+rm webtalk*.log
+rm webtalk*.jou
