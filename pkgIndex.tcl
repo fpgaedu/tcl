4581d77246897ec1a811ad80a2dc83a5deb61ee4
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2017-02-21 15:50:08 +0100

    Extended package index to include new packages

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,4 +1,6 @@
 package ifneeded fpgaedu::json 1.0 [list source [file join $dir json.tcl]]
+package ifneeded fpgaedu::json2 1.0 [list source [file join $dir json2.tcl]]
 package ifneeded fpgaedu::jsonrpc 1.0 [list source [file join $dir jsonrpc.tcl]]
 package ifneeded fpgaedu::assert 1.0 [list source [file join $dir assert.tcl]]
 package ifneeded fpgaedu::assert::json 1.0 [list source [file join $dir assertjson.tcl]]
+package ifneeded fpgaedu::vivado 1.0 [list source [file join $dir vivado.tcl]]
