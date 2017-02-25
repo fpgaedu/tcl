02fd7be970706c93eb7d3851ec815028f9d2a2fc
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2017-02-25 11:13:00 +0100

    remove json assert

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,4 @@
 package ifneeded fpgaedu::json 2.0 [list source [file join $dir json.tcl]]
 package ifneeded fpgaedu::jsonrpc 1.0 [list source [file join $dir jsonrpc.tcl]]
 package ifneeded fpgaedu::assert 1.0 [list source [file join $dir assert.tcl]]
-package ifneeded fpgaedu::assert::json 1.0 [list source [file join $dir assertjson.tcl]]
 package ifneeded fpgaedu::vivado 1.0 [list source [file join $dir vivado.tcl]]
