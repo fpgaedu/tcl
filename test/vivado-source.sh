1da9b4456d39245bc7c2b3be37ebf05f4f7a2b0f
Matthijs Bos <matthijs_vlaarbos@hotmail.com>
2017-02-26 22:37:30 +0100

    Prevent error output when cleaning up

--- test/vivado-source.sh
+++ test/vivado-source.sh
@@ -2,5 +2,6 @@
 vivado -mode batch -nolog -nojournal -notrace -source $@
 # Clean up webtalk log and journal. The generation of these files is 
 # unaffected by the -nolog and -nojournal command line arguments.
-rm webtalk*.log
-rm webtalk*.jou
+rm -f webtalk*.log
+rm -f webtalk*.jou
+rm -rf .Xil/
